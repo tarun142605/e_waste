8c2e91fa1d3a0c34a64c95ac291cbed2a901bf92
Shivam0802 <shivamk08001@gmail.com>
2024-02-27 12:46:11 +0530

    .

--- e_waste_backend/Routes/customerRoutes.js
+++ e_waste_backend/Routes/customerRoutes.js
@@ -1,10 +1,19 @@
 import express from 'express';
-import { registerCustomer, getCustomers, updateCustomer,deleteCustomer} from '../controllers/customerController.js';
 const router = express.Router();
+import Customer from '../DatabaseModels/customerModel.js';
 
-router.route('/').get(getCustomers);
-router.route('/register').post(registerCustomer);
-//router.route('/update').put(updateCustomer);
-//router.route('/delete').delete(deleteCustomer);
+// API for register customer
+router.post('/register', async (req, res) => {
+    let createdCustomer = await Customer.create(req.body);
+    res.send(createdCustomer);
+});
+
+//  API for get all customers
+router.get('/getCustomerByContact', async (req, res) => {
+    let customer = await Customer.findOne({ Contact: req.body.Contact });
+    res.send(customer);
+});
+
+//  API for update customer details
 
 export default router;
