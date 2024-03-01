dbfec48bec06beccc0124103e58fca35b98128a2
Naman Chauhan <namanchauhan2001@gmail.com>
2024-03-02 03:08:17 +0530

    customer controller updated

--- e_waste_backend/Controllers/customerController.js
+++ e_waste_backend/Controllers/customerController.js
@@ -1,6 +1,7 @@
 import asyncHandler from 'express-async-handler';
 import Customer from '../DatabaseModels/customerModel.js';
 
+var CustomerID;
 // Login a customer
 // POST /api/customers/login
 // Public
@@ -8,20 +9,17 @@ const loginCustomer = asyncHandler(async (req, res) => {
     let { Email, Password } = req.body;
     let customer = await Customer.findOne({ Email });
     if (customer && (customer.Password === Password)) {
+        CustomerID = customer._id;
         res.status(200).json({
             _id: customer._id,
-            Name: {
-                FirstName: customer.Name.FirstName,
-                LastName: customer.Name.LastName
-            },
+            FirstName: customer.FirstName,
+            LastName: customer.LastName,
             Email: customer.Email,
-            Address: {
-                HouseNo: customer.Address.HouseNo,
-                Street: customer.Address.Street,
-                City: customer.Address.City,
-                State: customer.Address.State,
-                Pincode: customer.Address.Pincode
-            },
+            HouseNo: customer.HouseNo,
+            Street: customer.Street,
+            City: customer.City,
+            State: customer.State,
+            Pincode: customer.Pincode,
             Contact: customer.Contact
         });
     } else {
@@ -48,18 +46,14 @@ const registerCustomer = asyncHandler(async (req, res) => {
     if (customer) {
         res.status(200).json({
             _id: customer._id,
-            Name: {
-                FirstName: customer.Name.FirstName,
-                LastName: customer.Name.LastName
-            },
+            FirstName: customer.FirstName,
+            LastName: customer.LastName,
             Email: customer.Email,
-            Address: {
-                HouseNo: customer.Address.HouseNo,
-                Street: customer.Address.Street,
-                City: customer.Address.City,
-                State: customer.Address.State,
-                Pincode: customer.Address.Pincode
-            },
+            HouseNo: customer.HouseNo,
+            Street: customer.Street,
+            City: customer.City,
+            State: customer.State,
+            Pincode: customer.Pincode,
             Contact: customer.Contact,
             Password: customer.Password
         });
@@ -78,18 +72,14 @@ let getCustomer = asyncHandler(async (req, res) => {
     let customer = await Customer.findOne(req.params.Email);
     if (customer) {
         res.json({
-            Name: {
-                FirstName: customer.Name.FirstName,
-                LastName: customer.Name.LastName
-            },
+            FirstName: customer.FirstName,
+            LastName: customer.LastName,
             Email: customer.Email,
-            Address: {
-                HouseNo: customer.Address.HouseNo,
-                Street: customer.Address.Street,
-                City: customer.Address.City,
-                State: customer.Address.State,
-                Pincode: customer.Address.Pincode
-            },
+            HouseNo: customer.HouseNo,
+            Street: customer.Street,
+            City: customer.City,
+            State: customer.State,
+            Pincode: customer.Pincode,
             Contact: customer.Contact
         });
     } else {
@@ -103,22 +93,17 @@ let getCustomer = asyncHandler(async (req, res) => {
 // Private
 
 const updateCustomer = asyncHandler(async (req, res) => {
-    let Email = 'abc@gmail.com';
-    let updatedCustomer = await Customer.findOneAndUpdate({Email}, {
-        Name: {
-            FirstName: req.body.FirstName,
-            LastName: req.body.LastName
-        },
-        Email : req.body.Email,
-        Address: {
-            HouseNo: req.body.HouseNo,
-            Street: req.body.Street,
-            City: req.body.City,
-            State: req.body.State,
-            Pincode: req.body.Pincode
-        },
-        Contact : req.body.Contact
-    }, {new : true});
+    let updatedCustomer = await Customer.findByIdAndUpdate(CustomerID, {
+        FirstName: req.body.FirstName,
+        LastName: req.body.LastName,
+        Email: req.body.Email,
+        HouseNo: req.body.HouseNo,
+        Street: req.body.Street,
+        City: req.body.City,
+        State: req.body.State,
+        Pincode: req.body.Pincode,
+        Contact: req.body.Contact
+    }, { new: true });
     res.status(200).json(updatedCustomer);
 });
 
@@ -127,7 +112,7 @@ const updateCustomer = asyncHandler(async (req, res) => {
 // DELETE /api/customers/:id
 // Private/Admin
 const deleteCustomer = asyncHandler(async (req, res) => {
-    let customer = await Customer.findOneAndDelete(req.params.Email);
+    let customer = await Customer.findByIdAndDelete(CustomerID);
     if (customer) {
         res.json({ message: 'Customer removed' });
     } else {
@@ -136,4 +121,4 @@ const deleteCustomer = asyncHandler(async (req, res) => {
     }
 });
 
-export { loginCustomer,registerCustomer, getCustomer,updateCustomer, deleteCustomer };
+export { loginCustomer, registerCustomer, getCustomer, updateCustomer, deleteCustomer };
--- e_waste_backend/DatabaseModels/customerModel.js
+++ e_waste_backend/DatabaseModels/customerModel.js
@@ -1,40 +1,37 @@
 import mongoose from "mongoose";
 const customerSchema = new mongoose.Schema({
-    Name: {
-        FirstName: {
-            type: String,
-            required: true
-        },
-        LastName: {
-            type: String,
-            required: true
-        }
+
+    FirstName: {
+        type: String,
+        required: true
+    },
+    LastName: {
+        type: String,
+        required: true
     },
     Email: {
         type: String,
         required: true
     },
-    Address: {
-        HouseNo: {
-            type: String,
-            required: true
-        },
-        Street: {
-            type: String,
-            required: true
-        },
-        City: {
-            type: String,
-            required: true
-        },
-        State: {
-            type: String,
-            required: true
-        },
-        Pincode: {
-            type: String,
-            required: true
-        }
+    HouseNo: {
+        type: String,
+        required: true
+    },
+    Street: {
+        type: String,
+        required: true
+    },
+    City: {
+        type: String,
+        required: true
+    },
+    State: {
+        type: String,
+        required: true
+    },
+    Pincode: {
+        type: String,
+        required: true
     },
     Contact: {
         type: Number,
